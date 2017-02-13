5c0ace68f00b6a92d7efc538718a82bfa11d6245
sajiner <739134362@qq.com>
2017-02-13 13:58:43 +0800

    first commit

---
+++ ZXRuntimeStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXRuntimeStudy
+//
+//  Created by sajiner on 2017/2/13.
+//  Copyright © 2017年 sajiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZXRuntimeStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZXRuntimeStudy
+//
+//  Created by sajiner on 2017/2/13.
+//  Copyright © 2017年 sajiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
